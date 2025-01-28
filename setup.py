c30a4d4daedc6f924dbb45fd4b07d4e576eb9668
benjamic <benjamin.czaja@surf.nl>
2025-01-28 15:26:53 +0100

    adding csv data

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     ],
     packages=[PACKAGE_NAME],
     py_modules=[PACKAGE_NAME],
+    package_data={'': ['eacctview/data/*.csv']},
     license='MIT',
     long_description=long_descrition,
     long_description_content_type="text/markdown",
